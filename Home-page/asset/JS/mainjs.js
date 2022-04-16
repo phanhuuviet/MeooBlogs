e28c9ca689a7cb9e4144a540729c75a7bde4cd12
Hòa Vũ <vuminhhoa.2002@gmail.com>
2022-04-16 15:56:47 +0700

    finished the javascript code for animation

--- Home-page/asset/JS/mainjs.js
+++ Home-page/asset/JS/mainjs.js
@@ -1,23 +1,86 @@
 
+const followedList = document.querySelector('.followed-list');
+const listNavbar = document.querySelector('.header__navbar-followed-list');
+const header = document.querySelector('.header');
+const headerTopSection = document.querySelector('.header__top-section');
+const headerItems = document.querySelectorAll('.header__top-item')
+const headerSearchIcon = document.querySelector('.header__top-item .header__search-icon');
+const headerCloseSearchButton = document.querySelector('.header__search-bar .header__search-bar__back-button') 
 
-(function clickToNavBar () {
-    const followBtn = document.querySelector('.header__navbar-list');
-    const listNavbar = document.querySelector('.header__navbar-followed-list');
-    let flag = false;
+let flag = false;
+let lastScrollValue = window.scrollY;
+function openLists() {
+    flag = !flag;
+    if(flag) {
+        listNavbar.classList.add('open');
+        listNavbar.style.animation = 'dropDown 0.3s ease-in-out';
 
-    function openLists () {
-        // if (!flag) {
-        //     listNavbar.classList.add('openFlex');
-        //     flag = true;
-        // }
-        // else {
-        //     listNavbar.classList.remove('openFlex');
-        //     flag = false;
-        // }
-        listNavbar.classList.toggle('openFlex');
+    } else {
+        listNavbar.style.animation = 'goUp 0.3s ease-in-out';
+        setTimeout(() => {
+            listNavbar.classList.remove('open');
+        }, 250);
     }
+}
 
-    followBtn.addEventListener('click', openLists);
-
+function navHandle() {
     
-}) ();
+    if(lastScrollValue < window.scrollY)
+    {
+        header.style.top = `-${headerTopSection.clientHeight}px`;
+    }
+    else {
+        header.style.top = 0;
+    }
+    
+    lastScrollValue = window.scrollY; 
+}
+
+function openSearchBar() {
+    headerItems.forEach(headerItem => {
+        if(headerItem.classList.contains('header__search-bar')) {
+            setTimeout(() => {
+                headerItem.style.display = 'flex';
+                headerItem.parentNode.style.flexDirection = 'row-reverse';
+
+                setTimeout(() => {
+                    headerItem.style.opacity = '1';
+                    headerItem.style.width = '100%';
+                },100); 
+            }, 200);
+            return;
+        }
+
+        headerItem.style.opacity = '0';
+        setTimeout(() => { 
+            headerItem.style.display = 'none';
+        }, 200)
+    })
+}
+
+function closeSearchBar() {
+    headerItems.forEach(headerItem => {
+        if(headerItem.classList.contains('header__search-bar')) {
+            headerItem.style.width = '40%';
+            headerItem.style.opacity = '0';
+
+            setTimeout(() => {
+                headerItem.parentNode.style.flexDirection = '';
+                headerItem.style.display = 'none';
+            }, 300)
+            return;
+        }
+
+        setTimeout(() => { 
+            headerItem.style.display = 'flex';
+            setTimeout(() => {
+                headerItem.style.opacity = '1';
+            },301)
+        }, 300)
+    });
+}
+
+headerCloseSearchButton.addEventListener('click', closeSearchBar)
+headerSearchIcon.addEventListener('click', openSearchBar);
+window.addEventListener('scroll', navHandle);
+followedList.addEventListener('click', openLists);
