b7a6a354c675c106d824d4fce5caf4d634e7ec0f
Hòa Vũ <vuminhhoa.2002@gmail.com>
2022-07-24 16:54:00 +0700

    finished writing page

--- writing-blog-page/assets/Javascript/main.js
+++ writing-blog-page/assets/Javascript/main.js
@@ -3,13 +3,15 @@ const selectedAll = document.querySelectorAll(".modal__selected-choice");
 const returnBtn = document.querySelector(".modal__return-btn");
 const confirmSubmitBtn = document.querySelector(".modal__submit-btn");
 const submitBtn = document.querySelector(".writing-blog__submit-btn");
-const modal = document.querySelector(".modal");
+const modalWritingPage = document.querySelector(".modal__writing-page-submition-form");
+const notificationIcon = document.querySelector(".header__icon__notificartion");
+const userAvatar = document.querySelector(".header__icon__user-navbar");
+const modalUserNavbar = document.querySelector(".header__modal__user-navbar");
+const modalNotification = document.querySelector(".header__modal__notification");
 
 var result = document.querySelector('blog-post-genre modal__selected-choice');
 
 const application = () => {
-    let currentLyVisible = false;
-
     dropDownList.forEach((modalSelect) => {
         //optionBox and selectBox is the same;
 
@@ -34,33 +36,60 @@ const application = () => {
             }
 
             modalSelect.querySelectorAll(".select-box__option")
+<<<<<<< HEAD
                 .forEach((option) => {
                     option.addEventListener("click", () => {
                         selectBox.innerHTML = option.querySelector("label").innerHTML;
                         // result = selectBox.textContent;
                         optionBox.classList.remove('active');
                     });
+=======
+            .forEach((option) => {
+                option.addEventListener("click", (e) => {
+                    // if(e.target.contains(label)) {
+                    //     console.log(true);
+                    // }
+                    // console.log(option);
+                    selectBox.innerHTML = option.querySelector("label").innerHTML;
+                    optionBox.classList.remove('active');
+>>>>>>> f6eb104 (finished writing page)
                 });
         });
 
         const makeModalHidden = () => {
+<<<<<<< HEAD
             modal.classList.remove('visible');
+=======
+            modalWritingPage.classList.remove('visible');
+>>>>>>> f6eb104 (finished writing page)
         }
 
         const makeModalVisible = () => {
-            modal.classList.add('visible');
+        modalWritingPage.classList.add('visible');
         }
 
         returnBtn.addEventListener('click', makeModalHidden);
         submitBtn.addEventListener('click', makeModalVisible);
     });
 
+<<<<<<< HEAD
+=======
+    const toggleUserNavbar = () => {
+        modalUserNavbar.classList.toggle('active');
+    };
+
+    const notificationToggle = () => {
+        modalNotification.classList.toggle('active');
+    }
+
+    userAvatar.addEventListener("click", toggleUserNavbar);
+    notificationIcon.addEventListener("click", notificationToggle);
+
+        
+>>>>>>> f6eb104 (finished writing page)
     BalloonEditor.create(document.querySelector(".editor"), {
         placeholder: "Nội dung bài viết",
     })
-        .then((editor) => {
-            console.log(editor);
-        })
         .catch((error) => {
             console.error(error);
         });
