52237ff4ca1d48754e36857adf04b1d4307c6afc
Hòa Vũ <vuminhhoa.2002@gmail.com>
2022-08-01 16:35:29 +0700

    small update

--- readingpage/asset/js/main.js
+++ readingpage/asset/js/main.js
@@ -8,4 +8,17 @@ const goToTop = function () {
     })
 };
 
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        prevArrow: $('.prev-btn'),
+        nextArrow: $('.next-btn'),
+    });
+});
+
+document.querySelector('.content-readpage').addEventListener("wheel", myFunction);
 arrowToTop.addEventListener("click", goToTop);
