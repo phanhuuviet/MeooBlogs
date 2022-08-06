a416bc36110f9af6d351d8e6d77eff9eeb588f2e
TrinhThuc <trinhthuc432@gmail.com>
2022-08-06 23:41:47 +0700

    update

--- Home-page/asset/JS/searchBtn.js
+++ Home-page/asset/JS/searchBtn.js
@@ -2,5 +2,5 @@ var searchBtn = document.querySelector('.header__search-bar .fas.fa-search')
 
 searchBtn.onclick = function () {
     var tab = document.querySelector('.header__search-bar > input').value
-    console.log(tab)
+    window.location.href = "/danh-sach/tim-kiem?tab="+tab+"&page=1&limit=5"
 }
