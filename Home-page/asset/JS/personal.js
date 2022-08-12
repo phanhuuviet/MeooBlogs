9105204eef5bd0b3a4475adc8f0f0c235ed2b1d6
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2022-08-12 18:08:56 +0700

    update responsive

--- Home-page/asset/JS/personal.js
+++ Home-page/asset/JS/personal.js
@@ -8,15 +8,12 @@ const chooseContents = document.querySelectorAll('.content__header-choose');
 const modalBtn = document.querySelector('.modal__btn');
 const modalChangePswBtn = document.querySelector('.modal__change-psw');
 const modalCancelBtn = document.querySelector('.modal__cancel-btn');
+const overlay = document.querySelector('.overlay');
 const modalEdit = document.querySelector('.modal__edit');
 const containerContent = document.querySelector('.container__content');
 const userEdit = document.querySelector('.user__edit');
 const userTable = document.querySelector('.container__user');
 const deleteBtn = document.querySelector('.table_icon .delete');
-
-// API
-
-// const urlPost = "http://localhost:3000/posts";
 
 const app = function () {
     let isOpen = false;
@@ -39,17 +36,27 @@ const app = function () {
             isOpen = !isOpen;
         }, 0.1);
 
-        // modalChangePswBtn.style.display = 'block';
     };
 
     const hideModalEdit = function () {
-        modalEdit.classList.remove('appear-block');
-        containerContent.classList.add('appear-block');
+        if (window.innerWidth < 1024) {
+            overlay.classList.remove('appear-flex');
+        }
+        else {
+            overlay.classList.remove('appear-block');
+            containerContent.classList.add('appear-block');
+        }
     };
 
     const hideContainerContent = function () {
-        modalEdit.classList.add('appear-block');
-        containerContent.classList.remove('appear-block');
+        if (window.innerWidth < 1024) {
+            overlay.classList.add('appear-flex');
+        }
+        else {
+            overlay.classList.add('appear-block');
+            containerContent.classList.remove('appear-block');
+        }
+        
     };
 
     const closeWhenClickOutside = (e) => {
@@ -62,6 +69,11 @@ const app = function () {
         }
     }
 
+    const hideOverlay = (e) => {
+        overlay.classList.remove('appear-flex');
+        e.stopPropagation();
+    }
+
     const toggleUserNavbar = () => {
         modalUserNavbar.classList.toggle("active");
     }
@@ -70,45 +82,9 @@ const app = function () {
         modalNotification.classList.toggle("active");
     }
 
-    // const getPost = (callback) => {
-    //     fetch(urlPost)
-    //         .then((response) => response.json())
-    //         .then(callback)
-    // }
-
-
-
-    // const renderPost = (posts) => {
-    //     const listPost = document.querySelector(".content__table");
-    //     var htmls = posts.map((post) => {
-    //         return `
-    //         <li id="content__table-item-${post.id}" class="content__table-item">
-    //                     <a href="" class="table__img-link">
-    //                         <img src="${post.img}" alt="">
-    //                     </a>
-    //                     <div class="table__content">
-    //                         <h4 class="table__tag">${post.category}
-    //                             <div class="table_icon">
-    //                                 <a href="" id="modify"><i class="fa fa-pen"></i></a>
-    //                                 <i onclick="deletePost(${post.id})" class="fa fa-trash-can"></i>
-    //                             </div>
-    //                         </h4>
-    //                         <h2 class="table__title">
-    //                             <a href="">
-    //                             ${post.title}
-    //                             </a>
-    //                         </h2>
-    //                         <div class="table__sub-title">
-    //                         ${post.description}
-    //                         </div>
-    //                         <div class="table__time">${post.time}</div>
-    //                     </div>
-    //                 </li>
-    //         `
-    //     });
-
-    //     listPost.innerHTML = htmls.join('');
-    // }
+    blockBubbleEvent = (e) => {
+        e.stopPropagation();
+    }
 
     if (userAvatar && notificationIcon) {
         document.addEventListener("click", closeWhenClickOutside)
@@ -126,28 +102,8 @@ const app = function () {
     modalBtn.addEventListener('click', styleChangePsw);
     modalCancelBtn.addEventListener('click', hideModalEdit);
     userEdit.addEventListener('click', hideContainerContent);
-    // getPost(renderPost);
+    overlay.addEventListener('click', hideOverlay);
+    modalEdit.addEventListener('click', blockBubbleEvent)
 }
-
-// const deletePost = (id) => {
-//     options = {
-//         method: 'DELETE',
-//         headers: {
-//             'Content-Type': 'application/json'
-//             // 'Content-Type': 'application/x-www-form-urlencoded',
-//         },
-//         // body: JSON.stringify(id)
-//     }
-//     fetch(urlPost + "/" + id, options)
-//         .then((response) => {
-//             return response.json();
-//         })
-//         .then(function () {
-//             var postItem = document.querySelector("#content__table-item-" + id);
-
-//             postItem.remove();
-//         })
-// }
-
 
 app();
